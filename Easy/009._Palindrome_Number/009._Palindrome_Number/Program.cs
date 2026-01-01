83e2da2ea20656a09221232b3e896797e15e4398
Arif Efe Ekinci <sirliefert54@gmail.com>
2026-01-01 23:07:02 +0300

    Organizing the setup..

---
+++ Easy/009._Palindrome_Number/009._Palindrome_Number/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009._Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Easy/009._Palindrome_Number/009._Palindrome_Number/Properties/AssemblyInfo.cs
+++ Easy/009._Palindrome_Number/009._Palindrome_Number/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BalancedBinaryTree")]
+[assembly: AssemblyTitle("009._Palindrome_Number")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BalancedBinaryTree")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyProduct("009._Palindrome_Number")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f5af9a8d-fcef-4ee5-9ef2-87df18d3f14a")]
+[assembly: Guid("d8d29622-8ad8-415c-993d-8806abbd117e")]
 
 // Version information for an assembly consists of the following four values:
 //
