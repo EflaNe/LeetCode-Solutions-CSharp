06febf58d3d86342fecc04989b94d41413598f75
Arif Efe Ekinci <sirliefert54@gmail.com>
2026-01-07 23:19:55 +0300

    https://leetcode.com/problems/valid-parentheses/

---
+++ EasyProblems/020_Valid_Parentheses.cs
@@ -0,0 +1,36 @@
+﻿namespace _0020_Valid_Parentheses
+{
+    // LeetCode: https://leetcode.com/problems/valid-parentheses/
+    // Difficulty: Easy
+    // Approach: Stack
+    // Time: O(n), Space: O(n)
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    char top = stack.Pop();
+
+                    if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '['))
+                        return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
